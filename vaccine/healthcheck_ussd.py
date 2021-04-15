f3697779a7b1ff7ef897d2ce1e84b73037d6bd73
Erik Harding <erik@praekelt.org>
2021-04-15 15:49:01 +0200

    add all ussd states

--- vaccine/healthcheck_ussd.py
+++ vaccine/healthcheck_ussd.py
@@ -1,4 +1,5 @@
 import logging
+import secrets
 from urllib.parse import urljoin
 
 import aiohttp
@@ -33,6 +34,16 @@ def get_eventstore():
     )
 
 
+def get_google_api():
+    return aiohttp.ClientSession(
+        timeout=aiohttp.ClientTimeout(total=5),
+        headers={
+            "Content-Type": "application/json",
+            "User-Agent": "Jsbox/Covid19-Triage-USSD",
+        },
+    )
+
+
 def get_turn():
     return aiohttp.ClientSession(
         timeout=aiohttp.ClientTimeout(total=5),
@@ -46,6 +57,66 @@ def get_turn():
 
 class Application(BaseApplication):
     START_STATE = "state_start"
+
+    def calculate_risk(self):
+        answers = self.user.answers
+
+        symptom_count = 0
+        for answer in [
+            answers.get("state_fever"),
+            answers.get("state_cough"),
+            answers.get("state_sore_throat"),
+            answers.get("state_breathing"),
+            answers.get("state_taste_and_smell"),
+        ]:
+            if answer == "yes":
+                symptom_count += 1
+
+        if answers.get("confirmed_contact") == "yes":
+            if answers.get("state_province") == "ZA-WC":
+                if (
+                    int(answers.get("state_age_years")) > 55
+                    or answers.get("state_preexisting_conditions") == "yes"
+                ) and symptom_count >= 1:
+                    return "high"
+                return "moderate"
+            if symptom_count >= 1:
+                return "high"
+            return "moderate"
+
+        if symptom_count == 0:
+            if answers.get("state_exposure") == "yes":
+                return "moderate"
+            return "low"
+
+        if symptom_count == 1:
+            if answers.get("state_exposure") == "yes":
+                return "high"
+            return "moderate"
+
+        if symptom_count == 2:
+            if answers.get("state_exposure") == "yes":
+                return "high"
+            if answers.get("state_age") == ">65":
+                return "high"
+            return "moderate"
+
+        return "high"
+
+    def pad_location(self, location, places):
+        # Pads the integer part of the number to places
+        # Ensures that there's always a sign
+        # Ensures that there's always a decimal part
+        sign = "+"
+        if location < 0:
+            sign = "-"
+            location = location * -1
+
+        location = location.split(".")[2]
+        integer = str(location[0])
+        decimal = location[1] or 0
+
+        return f"{sign}{integer.zfill(places)}.{decimal}"
 
     async def state_start(self):
         # TODO: normalise msisdn
@@ -103,6 +174,7 @@ class Application(BaseApplication):
         return await self.go_to_state("state_get_confirmed_contact")
 
     async def state_get_confirmed_contact(self):
+        self.save_answer("google_session_token", secrets.token_bytes(20).hex())
         # TODO: normalise msisdn
         msisdn = self.inbound.from_addr
         whatsapp_id = msisdn.lstrip("+")
@@ -275,6 +347,141 @@ class Application(BaseApplication):
             self, question=text, check=validate_city, next="state_google_places_lookup"
         )
 
+    async def state_google_places_lookup(self):
+        try:
+            response = await get_google_api().get(
+                "https://maps.googleapis.com/maps/api/place/autocomplete/json",
+                params={
+                    "input": self.user.answers.get("state_city"),
+                    "key": config.GOOGLE_PLACES_KEY,
+                    "sessiontoken": self.user.answers.get("google_session_token"),
+                    "language": "en",
+                    "components": "country:za",
+                },
+            )
+            response.raise_for_status()
+            data = await response.json()
+
+            if data["status"] != "OK":
+                return await self.go_to_state("state_city")
+
+            first_result = data["predictions"][0]
+            self.save_answer("place_id", first_result["place_id"])
+            self.save_answer("state_city", first_result["description"])
+
+            return await self.go_to_state("state_confirm_city")
+        except aiohttp.ClientError as e:
+            if i == 2:
+                logger.exception(e)
+                return await self.go_to_state("state_error")
+            else:
+                continue
+
+    async def state_confirm_city(self):
+        address = self.user.answers.get("state_city")[0, 160 - 79]
+        return MenuState(
+            self,
+            question="\n".join(
+                [
+                    "Please confirm the address below based on info you shared:",
+                    f"{{ address }}",
+                    "",
+                    "Reply",
+                ]
+            ),
+            choices=[
+                Choice("state_place_details_lookup", "Yes"),
+                Choice("state_city", "No"),
+            ],
+        )
+
+    async def state_place_details_lookup(self):
+        try:
+            response = await get_google_api().get(
+                "https://maps.googleapis.com/maps/api/place/details/json",
+                params={
+                    "key": config.GOOGLE_PLACES_KEY,
+                    "place_id": self.user.answers.get("place_id"),
+                    "sessiontoken": self.user.answers.get("google_session_token"),
+                    "language": "en",
+                    "fields": "geometry",
+                },
+            )
+            response.raise_for_status()
+            data = await response.json()
+
+            if data["status"] != "OK":
+                return await self.go_to_state("state_city")
+
+            location = data["result"]["geometry"]["location"]
+
+            lat = self.pad_location(location["lat"], 2)
+            lng = self.pad_location(location["lng"], 3)
+
+            self.save_answer("city_location", f"{lat}{lng}/")
+
+            if self.user.answers.get("confirmed_contact"):
+                return await self.go_to_state("state_tracing")
+            return await self.go_to_state("state_age")
+        except aiohttp.ClientError as e:
+            if i == 2:
+                logger.exception(e)
+                return await self.go_to_state("state_error")
+            else:
+                continue
+
+    async def state_age(self):
+        if self.user.answers.get("state_age"):
+            return await self.go_to_state("state_fever")
+
+        return ChoiceState(
+            self,
+            question="How old are you?",
+            error="\n".join(
+                [
+                    "Please use numbers from list.",
+                    "",
+                    "How old are you?",
+                ]
+            ),
+            choices=[
+                Choice("<18", "<18"),
+                Choice("18-40", "18-39"),
+                Choice("40-65", "40-65"),
+                Choice(">65", ">65"),
+            ],
+            next="state_fever",
+        )
+
+    async def state_age_years(self):
+        if self.user.answers.get("state_age") and self.user.answers.get(
+            "state_age_years"
+        ):
+            return await self.go_to_state("state_province")
+
+        def validate_age(content):
+            age = int(content)
+
+            if age < 1:
+                return question
+            elif age < 18:
+                self.save_answer("state_age", "<18")
+            elif age < 40:
+                self.save_answer("state_age", "18-40")
+            elif age <= 65:
+                self.save_answer("state_age", "40-65")
+            elif age < 150:
+                self.save_answer("state_age", ">65")
+            else:
+                return question
+
+        return FreeText(
+            self,
+            question="Please TYPE your age in years (eg. 35)",
+            check=validate_age,
+            next="state_province",
+        )
+
     async def state_fever(self):
         return ChoiceState(
             self,
@@ -298,6 +505,287 @@ class Application(BaseApplication):
             choices=[Choice("yes", "Yes"), Choice("no", "No")],
             next="state_cough",
         )
+
+    async def state_cough(self):
+        question = "\n".join(
+            [
+                "Do you feel very hot or cold? Are you sweating or shivering? When "
+                "you touch your forehead, does it feel hot?",
+                "",
+                "Reply",
+            ]
+        )
+        error = "\n".join(
+            [
+                "Please use numbers from list. Do you feel very hot or cold? Are "
+                "you sweating or shivering? When you touch your forehead, does it "
+                "feel hot?",
+                "",
+                "Reply",
+            ]
+        )
+        if self.user.answers.get("confirmed_contact") == "yes":
+            question = "\n".join(
+                [
+                    "Do you have a cough that recently started in the last week?",
+                    "",
+                    "Reply",
+                ]
+            )
+            error = "\n".join(
+                [
+                    "This service works best when you select numbers from the list.",
+                    "",
+                    "Do you have a cough that recently started in the last week?",
+                    "",
+                    "Reply",
+                ]
+            )
+        return ChoiceState(
+            self,
+            question=question,
+            error=error,
+            choices=[Choice("yes", "Yes"), Choice("no", "No")],
+            next="state_sore_throat",
+        )
+
+    async def state_sore_throat(self):
+        return ChoiceState(
+            self,
+            question="\n".join(
+                ["Do you have a sore throat, or pain when swallowing?", "", "Reply"]
+            ),
+            error="\n".join(
+                [
+                    "Please use numbers from list.",
+                    "Do you have a sore throat, or pain when swallowing?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            choices=[Choice("yes", "Yes"), Choice("no", "No")],
+            next="state_breathing",
+        )
+
+    async def state_breathing(self):
+        question = "\n".join(
+            [
+                "Do you have breathlessness or a difficulty breathing, that you've noticed recently?",
+                "Reply",
+            ]
+        )
+        error = "\n".join(
+            [
+                "Please use numbers from list. Do you have breathlessness or a difficulty breathing, "
+                + "that you've noticed recently?",
+                "Reply",
+            ]
+        )
+        next_state = "state_exposure"
+        if self.user.answers.get("confirmed_contact") == "yes":
+            question = "\n".join(
+                [
+                    "Do you have shortness of breath while resting or difficulty breathing, that you've "
+                    + "noticed recently?",
+                    "",
+                    "Reply",
+                ]
+            )
+            error = "\n".join(
+                [
+                    "Please use numbers from list.",
+                    "",
+                    "Do you have shortness of breath while resting or difficulty breathing, that you've "
+                    + "noticed recently?",
+                    "",
+                    "Reply",
+                ]
+            )
+            self.save_answer("state_exposure", "yes")
+            next_state = "state_taste_and_smell"
+        return ChoiceState(
+            self,
+            question=question,
+            error=error,
+            choices=[Choice("yes", "Yes"), Choice("no", "No")],
+            next=next_state,
+        )
+
+    async def state_taste_and_smell(self):
+        return ChoiceState(
+            self,
+            question="\n".join(
+                [
+                    "Have you noticed any recent changes in your ability to taste or smell things?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            error="\n".join(
+                [
+                    "This service works best when you select numbers from the list.",
+                    "Have you noticed any recent changes in your ability to taste or smell things?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            choices=[Choice("yes", "Yes"), Choice("no", "No")],
+            next="state_preexisting_conditions",
+        )
+
+    async def state_preexisting_conditions(self):
+        if self.user.answers.get("state_preexisting_conditions"):
+            return await self.go_to_state("state_age_years")
+
+        return ChoiceState(
+            self,
+            question="\n".join(
+                [
+                    "Have you been diagnosed with either Obesity, Diabetes, Hypertension or Cardiovascular "
+                    + "disease?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            error="\n".join(
+                [
+                    "Please use numbers from list.",
+                    "",
+                    "Have you been diagnosed with either Obesity, Diabetes, Hypertension or Cardiovascular "
+                    + "disease?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            choices=[
+                Choice("yes", "YES"),
+                Choice("no", "NO"),
+                Choice("not_sure", "NOT SURE"),
+            ],
+            next="state_age_years",
+        )
+
+    async def state_exposure(self):
+        return ChoiceState(
+            self,
+            question="\n".join(
+                [
+                    "Have you been in close contact to someone confirmed to be infected with COVID19?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            error="\n".join(
+                [
+                    "Please use numbers from list. Have u been in contact with someone with COVID19 or "
+                    + "been where COVID19 patients are treated?",
+                    "",
+                    "Reply",
+                ]
+            ),
+            choices=[
+                Choice("yes", "Yes"),
+                Choice("no", "No"),
+                Choice("not_sure", "NOT SURE"),
+            ],
+            next="state_tracing",
+        )
+
+    async def state_tracing(self):
+        question = "\n".join(
+            [
+                "Please confirm that the information you shared is correct & that the National "
+                + "Department of Health can contact you if necessary?",
+                "",
+                "Reply",
+            ]
+        )
+        error = "\n".join(
+            [
+                "Please reply with numbers",
+                "Is the information you shared correct & can the National Department of Health contact "
+                + "you if necessary?",
+                "",
+                "Reply",
+            ]
+        )
+        choices = [Choice("yes", "YES"), Choice("no", "NO"), Choice(None, "RESTART")]
+        if self.user.answers.get("confirmed_contact") == "yes":
+            question = "\n".join(
+                [
+                    "Finally, please confirm that the information you shared is ACCURATE to the best of "
+                    + "your knowledge?",
+                    "",
+                    "Reply",
+                ]
+            )
+            error = "\n".join(
+                [
+                    "Please use numbers from the list.",
+                    "",
+                    "Finally, please confirm that the information you shared is ACCURATE to the best of "
+                    + "your knowledge?",
+                    "",
+                    "Reply",
+                ]
+            )
+            choices = [
+                Choice("yes", "YES"),
+                Choice("no", "NO"),
+            ]
+        return ChoiceState(
+            self,
+            question=question,
+            error=error,
+            choices=choices,
+            next="state_submit_data",
+        )
+
+    async def state_submit_data(self):
+        for i in range(3):
+            try:
+                response = await get_eventstore().post(
+                    urljoin(
+                        config.EVENTSTORE_API_URL,
+                        f"/api/v3/covid19triage/",
+                    ),
+                    json={
+                        "msisdn": self.inbound.from_addr,
+                        "source": f"USSD {self.inbound.to_addr}",
+                        "province": self.user.answers.get("state_province"),
+                        "city": self.user.answers.get("state_city"),
+                        "city_location": self.user.answers.get("city_location"),
+                        "age": self.user.answers.get("state_age"),
+                        "fever": self.user.answers.get("state_fever"),
+                        "cough": self.user.answers.get("state_cough"),
+                        "sore_throat": self.user.answers.get("state_sore_throat"),
+                        "difficulty_breathing": self.user.answers.get(
+                            "state_breathing"
+                        ),
+                        "smell": self.user.answers.get("state_taste_and_smell"),
+                        "preexisting_condition": self.user.answers.get(
+                            "state_preexisting_conditions"
+                        ),
+                        "exposure": self.user.answers.get("state_exposure"),
+                        "tracing": self.user.answers.get("state_tracing"),
+                        "confirmed_contact": self.user.answers.get("confirmed_contact"),
+                        "risk": self.calculate_risk(),
+                        "data": {"age_years": self.user.answers.get("state_age_years")},
+                    },
+                )
+                if response.status == 404:
+                    self.save_answer("returning_user", "no")
+                    return await self.go_to_state("state_save_healthcheck_start")
+                response.raise_for_status()
+                data = await response.json()
+                break
+            except aiohttp.ClientError as e:
+                if i == 2:
+                    logger.exception(e)
+                    return await self.go_to_state("state_error")
+                else:
+                    continue
+        return await self.go_to_state("state_display_risk")
 
     async def state_error(self):
         return EndState(
