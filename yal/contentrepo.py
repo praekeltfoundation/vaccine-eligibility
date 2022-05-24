85295be56b28d50ea107e12fd7f5123787bd981e
Erik Harding <erik@praekelt.org>
2022-05-24 08:30:04 +0200

    Add start of mainmenu+contentrepo integration

---
+++ yal/contentrepo.py
@@ -0,0 +1,85 @@
+import logging
+from urllib.parse import urljoin
+
+import aiohttp
+
+from vaccine.states import Choice
+from vaccine.utils import HTTP_EXCEPTIONS
+from yal import config
+
+logger = logging.getLogger(__name__)
+
+
+def get_contentrepo_api():
+    # TODO: Cache the session globally. Things that don't work:
+    # - Declaring the session at the top of the file
+    #   You get a `Timeout context manager should be used inside a task` error
+    # - Declaring it here but caching it in a global variable for reuse
+    #   You get a `Event loop is closed` error
+    return aiohttp.ClientSession(
+        timeout=aiohttp.ClientTimeout(total=5),
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "User-Agent": "yal-whatsapp-bot",
+        },
+    )
+
+
+async def get_choices_by_tag(tag):
+    return await get_choices_by_path(f"/api/v2/pages?tag={tag}")
+
+
+async def get_choices_by_parent(parent_id):
+    return await get_choices_by_path(f"/api/v2/pages?child_of={parent_id}")
+
+
+async def get_choices_by_path(path):
+    choices = []
+    async with get_contentrepo_api() as session:
+        for i in range(3):
+            try:
+                response = await session.get(urljoin(config.CONTENTREPO_API_URL, path))
+                response.raise_for_status()
+                response_body = await response.json()
+
+                for page in response_body["results"]:
+                    choices.append(Choice(str(page["id"]), page["title"]))
+
+                break
+            except HTTP_EXCEPTIONS as e:
+                if i == 2:
+                    logger.exception(e)
+                    return True, []
+                else:
+                    continue
+    return False, choices
+
+
+async def get_page_details(page_id):
+    page_details = {}
+    async with get_contentrepo_api() as session:
+        for i in range(3):
+            try:
+                response = await session.get(
+                    urljoin(
+                        config.CONTENTREPO_API_URL,
+                        f"/api/v2/pages/{page_id}?whatsapp=true",
+                    )
+                )
+                response.raise_for_status()
+                response_body = await response.json()
+
+                page_details["has_children"] = response_body["has_children"]
+                page_details["title"] = response_body["title"]
+                page_details["body"] = response_body["body"]["text"]["value"]["message"]
+                # TODO: handle multiple messages on one page.
+
+                break
+            except HTTP_EXCEPTIONS as e:
+                if i == 2:
+                    logger.exception(e)
+                    return True, []
+                else:
+                    continue
+    return False, page_details
--- yal/mainmenu.py
+++ yal/mainmenu.py
@@ -1,13 +1,104 @@
-from vaccine.states import EndState
+import logging
+
+from vaccine.states import Choice, ChoiceState, EndState
+from yal import contentrepo
 from yal.yal_base_application import YalBaseApplication
+
+logger = logging.getLogger(__name__)
 
 
 class Application(YalBaseApplication):
     START_STATE = "state_mainmenu"
 
     async def state_mainmenu(self):
+        async def next_(choice: Choice):
+            if choice.value.startswith("state_"):
+                return choice.value
+            else:
+                self.save_answer("selected_page_id", choice.value)
+                return "state_contentrepo_page"
+
+        before_choices = [
+            Choice("state_please_call_me", "📞 Please call me!"),
+        ]
+        error, contentrepo_choices = await contentrepo.get_choices_by_tag("mainmenu")
+        if error:
+            return await self.go_to_state("state_error")
+
+        after_choices = [
+            Choice("state_change_info", "⚙️ Change/Update Your Personal Info"),
+        ]
+
+        return ChoiceState(
+            self,
+            question=self._("*Main Menu*\n" "\n" "Welcome"),
+            error=self._(
+                "⚠️ This service works best when you use the numbered options "
+                "available\n"
+            ),
+            error_footer=self._(
+                "\n"
+                "-----\n"
+                "Or reply 📌 *0* to end this session and return to the main *MENU*"
+            ),
+            choices=[*before_choices, *contentrepo_choices, *after_choices],
+            next=next_,
+        )
+
+    async def state_contentrepo_page(self):
+        page_id = self.user.answers["selected_page_id"]
+        error, page_details = await contentrepo.get_page_details(page_id)
+        if error:
+            return await self.go_to_state("state_error")
+
+        title = page_details["title"]
+        body = page_details["body"]
+        self.save_answer("question", self._(f"{title}\n\n{body}"))
+
+        if page_details["has_children"]:
+            return await self.go_to_state("state_submenu")
+        else:
+            return await self.go_to_state("state_detail")
+
+    async def state_submenu(self):
+        async def next_(choice: Choice):
+            self.save_answer("selected_page_id", choice.value)
+            return "state_contentrepo_page"
+
+        page_id = self.user.answers["selected_page_id"]
+        error, choices = await contentrepo.get_choices_by_parent(page_id)
+        if error:
+            return await self.go_to_state("state_error")
+
+        return ChoiceState(
+            self,
+            question=self.user.answers["question"],
+            error=self._(
+                "⚠️ This service works best when you use the numbered options "
+                "available\n"
+            ),
+            choices=choices,
+            next=next_,
+        )
+
+    async def state_detail(self):
+        return EndState(
+                self,
+                self.user.answers["question"],
+                next=self.START_STATE,
+            )
+
+
+    async def state_please_call_me(self):
         return EndState(
             self,
-            self._("TODO: Main Menu"),
+            self._("TODO: Please Call Me"),
+            next=self.START_STATE,
+        )
+
+    async def state_change_info(self):
+        return EndState(
+            self,
+            self._("TODO: Change info/preferences"),
             next=self.START_STATE,
         )
