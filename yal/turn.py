0014d0648d2fa47224d11128fa5030797d37395f
Erik Harding <erik@praekelt.org>
2022-05-31 12:29:43 +0200

    Fix turn url for change prefs flow

--- yal/turn.py
+++ yal/turn.py
@@ -1,5 +1,5 @@
 import logging
-from urllib.parse import urljoin
+from urllib.parse import ParseResult, urlunparse
 
 from vaccine.utils import HTTP_EXCEPTIONS
 from yal import config, utils
@@ -7,14 +7,25 @@ from yal import config, utils
 logger = logging.getLogger(__name__)
 
 
+def get_profile_url(whatsapp_id):
+    return urlunparse(
+        ParseResult(
+            scheme="https",
+            netloc=config.API_HOST or "",
+            path=f"/v1/contacts/{whatsapp_id}/profile",
+            params="",
+            query="",
+            fragment="",
+        )
+    )
+
+
 async def get_profile(whatsapp_id):
     fields = {}
     async with utils.get_turn_api() as session:
         for i in range(3):
             try:
-                response = await session.get(
-                    urljoin(config.API_HOST, f"/v1/contacts/{whatsapp_id}/profile")
-                )
+                response = await session.get(get_profile_url(whatsapp_id))
                 response.raise_for_status()
                 response_body = await response.json()
 
@@ -34,9 +45,7 @@ async def update_profile(whatsapp_id, data):
     async with utils.get_turn_api() as session:
         for i in range(3):
             try:
-                response = await session.patch(
-                    urljoin(config.API_HOST, f"/v1/contacts/{whatsapp_id}/profile")
-                )
+                response = await session.patch(get_profile_url(whatsapp_id))
                 response.raise_for_status()
                 break
             except HTTP_EXCEPTIONS as e:
