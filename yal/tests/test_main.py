8b2d010a5a8bf4573fce01315969e24abf74e7f6
Kaitlyn <kaitlyn@praekelt.org>
2022-07-15 20:28:28 +0200

    YAL: in main.py route please call me reminder responses

--- yal/tests/test_main.py
+++ yal/tests/test_main.py
@@ -51,6 +51,7 @@ def get_rapidpro_contact(urn):
             "prototype_user": "27820001001" in urn,
             "onboarding_completed": "27820001001" in urn,
             "onboarding_reminder_sent": "27820001001" in urn,
+            "callback_check_sent": "27820001001" in urn,
             "terms_accepted": "27820001001" in urn,
             "province": "FS",
             "suburb": "cape town",
@@ -203,6 +204,17 @@ async def test_onboarding_reminder_response_to_reminder_handler(
 ):
     await tester.user_input(session=Message.SESSION_EVENT.NEW, content="not interested")
     tester.assert_state("state_stop_onboarding_reminders")
+    tester.assert_num_messages(1)
+
+    assert len(rapidpro_mock.app.requests) == 2
+
+
+@pytest.mark.asyncio
+async def test_callback_check_response_to_handler(
+    tester: AppTester, rapidpro_mock
+):
+    await tester.user_input(session=Message.SESSION_EVENT.NEW, content="yes but I missed it")
+    tester.assert_state("state_ask_to_call_again")
     tester.assert_num_messages(1)
 
     assert len(rapidpro_mock.app.requests) == 2
