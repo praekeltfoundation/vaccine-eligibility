dcd7faa7acd16bff0447ff3ef79d6e888992fd68
Kaitlyn <kaitlyn@praekelt.org>
2022-10-10 17:57:52 +0200

    YAL: fix replace_persona_fields method (again)

--- yal/utils.py
+++ yal/utils.py
@@ -70,7 +70,7 @@ def normalise_phonenumber(phonenumber):
 def replace_persona_fields(text, metadata={}):
     for key in PERSONA_FIELDS:
         if key in metadata:
-            return text.replace(f"[{key}]", metadata[key])
+            text = text.replace(f"[{key}]", metadata[key])
         else:
             text = text.replace(f"[{key}]", PERSONA_DEFAULTS[key])
     return text
