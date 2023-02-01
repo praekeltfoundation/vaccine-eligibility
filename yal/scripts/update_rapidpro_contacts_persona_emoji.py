425d39ca303b95b063f1fcbe5578aadc484b3bee
megh-k <meghan@praekelt.org>
2023-02-01 15:33:52 +0200

    updates to rapidpro update script

--- yal/scripts/update_rapidpro_contacts_persona_emoji.py
+++ yal/scripts/update_rapidpro_contacts_persona_emoji.py
@@ -9,10 +9,9 @@ from yal.utils import extract_first_emoji
 
 def get_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="Pretends to be a webhook from whatsapp for a template message "
-        "button click with payload"
+        description="Checks for contacts in rapidpro with invalid persona_emojis"
     )
-    parser.add_argument("url", help="Base URL for the transport")
+    parser.add_argument("url", help="Rapidpro url")
     parser.add_argument("api_token", help="Rapidpro API token")
 
     return parser.parse_args()
@@ -22,36 +21,48 @@ def update_contacts(arguments: argparse.Namespace):
     rapidpro_base_api_url = arguments.url
     api_token = arguments.api_token
 
-    response = requests.get(
-        f"{rapidpro_base_api_url}/api/v2/contacts.json",
-        headers={"Authorization": f"Token {api_token}"},
-    )
-    response = response.json()
+    list_contacts_url = f"{rapidpro_base_api_url}/api/v2/contacts.json"
 
-    for contact in response["results"]:
-        if (
-            not emoji.is_emoji(contact["fields"]["persona_emoji"])
-            and contact["fields"]["persona_emoji"] != "Skip"
-            and contact["fields"]["persona_emoji"] is not None
-        ):
-            new_persona_emoji = extract_first_emoji(contact["fields"]["persona_emoji"])
+    while list_contacts_url:
+        response = requests.get(
+            list_contacts_url,
+            headers={"Authorization": f"Token {api_token}"},
+        ).json()
 
-            payload = json.dumps({"fields": {"persona_emoji": new_persona_emoji}})
-            headers = {
-                "Authorization": f"Token {api_token}",
-                "Content-Type": "application/json",
-            }
+        for contact in response["results"]:
+            if (
+                not emoji.is_emoji(contact["fields"]["persona_emoji"])
+                and contact["fields"]["persona_emoji"] is not None
+                and contact["fields"]["persona_emoji"].lower() != "skip"
+            ):
+                new_persona_emoji = extract_first_emoji(
+                    contact["fields"]["persona_emoji"]
+                )
+                if contact["fields"]["persona_emoji"] == new_persona_emoji:
+                    continue
 
-            post_response = requests.post(
-                f"{rapidpro_base_api_url}/api/v2/contacts.json?uuid={contact['uuid']}",
-                headers=headers,
-                data=payload,
-            )
-            if post_response.status_code == 200:
-                print(f"success {contact['uuid']}")
-            else:
-                print(f"failed {contact['uuid']}")
-                print(post_response.content)
+                payload = json.dumps({"fields": {"persona_emoji": new_persona_emoji}})
+                headers = {
+                    "Authorization": f"Token {api_token}",
+                    "Content-Type": "application/json",
+                }
+
+                post_response = requests.post(
+                    "".join(
+                        [
+                            f"{rapidpro_base_api_url}"
+                            f"/api/v2/contacts.json?uuid={contact['uuid']}"
+                        ]
+                    ),
+                    headers=headers,
+                    data=payload,
+                )
+                if post_response.status_code == 200:
+                    print(f"success {contact['uuid']}")
+                else:
+                    print(f"failed {contact['uuid']}")
+                    print(post_response.content)
+        list_contacts_url = response["next"]
 
 
 def main():
