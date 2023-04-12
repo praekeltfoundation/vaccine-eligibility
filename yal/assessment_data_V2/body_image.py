24d300a25a32dbc9d9d775776db79b22e7bed43b
Fritz Brand <fritz@praekelt.org>
2023-04-12 08:03:12 +0200

    Added Body Image part of survey

---
+++ yal/assessment_data_V2/body_image.py
@@ -0,0 +1,60 @@
+# Survey state naming convention
+# [survey_name]_[subsection]_q[question]_topic
+ASSESSMENT_QUESTIONS = {
+    "1":
+    {
+        "start": "baseline_3_q1_body_image",
+        "questions": {
+            "baseline_3_q1_body_image": {
+                "type": "list",
+                "text": "\n".join(
+                    [
+                        "*Do you agree with this statement?*",
+                        "",
+                        "I feel good about myself.",
+                    ]
+                ),
+                "options": [
+                    "Yes",
+                    "No",
+                    "Sometimes",
+                    "I don't understand",
+                    "Skip"
+                ],
+                "scoring": {
+                    "yes": 3, 
+                    "no": 1, 
+                    "sometimes": 2,
+                    "dont_understand": 1,
+                    "skip": 1,
+                },
+                "next": "baseline_3_q2_body_image",
+            },
+            "baseline_3_q2_body_image": {
+                "type": "list",
+                "text": "\n".join(
+                    [
+                         "*Do you agree with this statement?*",
+                        "",
+                        "I feel good about my body.",
+                    ]
+                ),
+                "options": [
+                    "Yes",
+                    "No",
+                    "Sometimes",
+                    "I don't understand",
+                    "Skip"
+                ],
+                "scoring": {
+                    "yes": 3, 
+                    "no": 1, 
+                    "sometimes": 2,
+                    "dont_understand": 1,
+                    "skip": 1,
+                },
+                "next": None,
+            },
+        },
+    },
+}
