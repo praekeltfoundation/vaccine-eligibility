18db0bbb69bd79acf89dfd6bc415b2ada76cf004
Fritz Brand <fritz@praekelt.org>
2023-04-13 14:01:29 +0200

    Fixed copy error

--- yal/assessments.py
+++ yal/assessments.py
@@ -85,7 +85,7 @@ QUESTIONS = {
     "self_perceived_healthcare_v2": SELF_PERCEIVED_HEALTHCARE_QUESTIONS_V2,
     "sexual_health_literacy_v2": SEXUAL_HEALTH_LITERACY_QUESTIONS_V2,
     "gender_attitude_v2": GENDER_ATTITUDE_QUESTIONS_V2,
-    "connectedness_v2": SEXUAL_CONSENT_QUESTIONS_V2,
+    "sexual_consent_v2": SEXUAL_CONSENT_QUESTIONS_V2,
     "alcohol_v2": ALCOHOL_QUESTIONS_V2,
 }
 
