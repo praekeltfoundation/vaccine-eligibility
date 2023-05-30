e5d08d8e7b847f8e67d3d0a67eb5b94c13011402
Buhle <buhle@praekelt.org>
2023-05-30 16:05:48 +0200

    Added a global variable to check if service finder is active to added find clinic and service to the menu

--- yal/rapidpro.py
+++ yal/rapidpro.py
@@ -161,3 +161,34 @@ async def check_if_baseline_active():
                 else:
                     continue
     return is_baseline_survey_active
+
+
+async def check_if_service_finder_active():
+    """
+    Checks a Global var on the RapidPro instance to see if the Service finder is active
+    """
+    async with get_rapidpro_api() as session:
+        is_service_finder_active = False
+        for i in range(3):
+            try:
+                response = await session.get(
+                    urljoin(
+                        config.RAPIDPRO_URL,
+                        "/api/v2/globals.json?key=service_finder_active",
+                    ),
+                )
+                print(">>>>>", response)
+                response.raise_for_status()
+                response_body = await response.json()
+
+                if len(response_body["results"]) > 0:
+                    is_service_finder_active = response_body["results"][0]["value"]
+                response.close()
+                break
+            except HTTP_EXCEPTIONS as e:
+                if i == 2:
+                    logger.exception(e)
+                    return True
+                else:
+                    continue
+    return is_service_finder_active
