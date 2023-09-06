da23aeae29830902b88f87720573ec5bf30e968e
Hlamalani <hlamalani@praekelt.org>
2023-09-06 12:43:14 +0200

    check if service finder is active

--- yal/contentrepo.py
+++ yal/contentrepo.py
@@ -7,7 +7,7 @@ import aiohttp
 from vaccine.models import User
 from vaccine.states import Choice
 from vaccine.utils import HTTP_EXCEPTIONS
-from yal import config
+from yal import config, rapidpro
 
 logger = logging.getLogger(__name__)
 
@@ -166,7 +166,15 @@ async def get_page_details(
                             ][0]
                             page_details["quiz_tag"] = quiz_tag
 
-                        for tag in ["servicefinder", "aaq", "pleasecallme"]:
+                        tags = ["aaq", "pleasecallme"]
+
+                        if (
+                            await rapidpro.get_global_flag("service_finder_active")
+                            is True
+                        ):
+                            tags.append("servicefinder")
+
+                        for tag in tags:
                             if tag in page_details["tags"]:
                                 page_details["feature_redirects"].append(tag)
 
